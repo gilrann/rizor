e8562969a70555a7b816eb3111d2f5737415f1f0
gilrann <gilrann@users.github.com>
2019-05-12 22:45:27 +0700

    comm

--- js/rizor.js
+++ js/rizor.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+    $("#pagination").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+
+
+  $(".pagination__link").click(function(e) {
+    e.preventDefault();
+    $(".pagination__link").removeClass('active');
+    $(this).addClass('active');
+  })
+
+
 });
